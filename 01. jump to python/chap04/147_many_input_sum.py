52f3f0feab13c82b71b424233bea20064051551a
Yoonminsun <alstjs7321@naver.com>
2019-02-07 17:21:49 +0900

    집에서 수정1

--- 01. jump to python/chap04/147_many_input_sum.py
+++ 01. jump to python/chap04/147_many_input_sum.py
@@ -5,6 +5,6 @@ def sum(*args):
     return sum
 
 tu = list(map(int,input().split())) # 공백기준으로 split하여 int 타입으로 리스트를 만들어 맵핑, 리스트 생성
-# tu = tuple(tu) 리스트를 튜플로 바꾸고 싶은 경우
+# list 대신 tuple로 하면 튜플로 입력
 print(tu) # 리스트,튜플 모두 *args에 들어갈 수 있으므로 리스트 그래도 사용 가능
 print(sum(*tu)) # 리스트,튜플을 넣을시 *을 붙여 언패킹(unpacking) 한다
--- 01. jump to python/chap04/150_return_tuple_input.py
+++ 01. jump to python/chap04/150_return_tuple_input.py
@@ -7,6 +7,6 @@ def sum_and_mul(*args):
     return sum,mul # 튜플 형태로 return 된다
 
 
-input_list = list(map(int,input("리스트 입력:").split())) #리스트로 입력받음
+input_list = list(map(int,input("리스트 입력:").split())) #리스트로 입력받음,list 대신 tuple로 하면 튜플로 입력
 sum,mul=sum_and_mul(*input_list) # 입력받은 리스트를 인자로 함수 호출하여 튜플값 각각 대입
 print("sum= %d mul=%d"%(sum,mul)) # 출력
