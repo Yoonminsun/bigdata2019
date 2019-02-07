afb8e2b3ad201a9b3439e0659312c14708765790
Yoonminsun <alstjs7321@naver.com>
2019-02-07 16:54:56 +0900

    집에서 수정1

--- 01. jump to python/chap04/147_many_input_sum.py
+++ 01. jump to python/chap04/147_many_input_sum.py
@@ -4,7 +4,7 @@ def sum(*args):
         sum+=i
     return sum
 
-tu = list(map(int,input().split()))
-# tu = tuple(tu)
-print(tu)
-print(sum(*tu))
+tu = list(map(int,input().split())) # 공백기준으로 split하여 int 타입으로 리스트를 만들어 맵핑, 리스트 생성
+# tu = tuple(tu) 리스트를 튜플로 바꾸고 싶은 경우
+print(tu) # 리스트,튜플 모두 *args에 들어갈 수 있으므로 리스트 그래도 사용 가능
+print(sum(*tu)) # 리스트,튜플을 넣을시 *을 붙여 언패킹(unpacking) 한다
