83a2bc14f8ed482a54ed72f518799ac602f08162
Yoonminsun <alstjs7321@naver.com>
2019-02-11 09:58:05 +0900

    집에서 수정1

--- 01. jump to python/chap04/test.py
+++ 01. jump to python/chap04/test.py
@@ -1,24 +1,12 @@
-class HousePark:
+class Test:
+   # name=''
+   #  def setname(self,name):
+   #      self.name = name
+   def __init__(self,name):
+       self.name = name
 
-    lastname = '박'
-    def __init__(self,name):
-        self.fullname = self.lastname+name
-    def travel(self,where):
-        print("%s, %s여행을 가다."%(self.fullname,where))
-
-class HouseKim(HousePark):
-
-    lastname = '김'
-    def travel(self,where,day):
-        print("%s, %s여행을 %d일 가다"%(self.fullname,where,day))
-
-my = HousePark("응용")
-print(my.lastname)
-print(my.fullname)
-my.travel("부산")
-
-print('')
-my2 = HouseKim("줄리엣")
-print(my2.lastname)
-print(my2.fullname)
-my2.travel("독도",3)
+my = Test('aa')
+# my.name='aa'
+# print(my.name)
+# my.setname('aa')
+print(my.name)
