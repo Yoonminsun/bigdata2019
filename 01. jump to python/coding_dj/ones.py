ac5f99036f81fdfebffe15f653ce6f2d100b8263
Yoonminsun <alstjs7321@naver.com>
2019-02-15 17:30:11 +0900

    집에서 수정1

--- 01. jump to python/coding_dj/ones.py
+++ 01. jump to python/coding_dj/ones.py
@@ -1,14 +1,15 @@
 mul_n=0
 n_break=0
-n= int(input())
+n= input()
 print(n)
 
 while True:
-    for i in str(n):
+    for i in n:
         if i!='1':
             n_break=1
             break
     if n_break==0:
         print(len(n))
         break
-    n=n*n
+
+    n=str(int(n)*int(n))
