fc59abe1760fecd44369ef882340e60f208cff35
Yoonminsun <alstjs7321@naver.com>
2019-02-20 17:28:12 +0900

    집에서 수정1

--- 01. jump to python/chap07/2_XML/test.py
+++ 01. jump to python/chap07/2_XML/test.py
@@ -1,15 +1,17 @@
 from xml.etree.ElementTree import parse
+import re
 
 menu=0
 tree = parse('students_info.xml')
 note = tree.getroot()
 
-# child = note.getiterator()
-# print(list(child))
-# child2 = child.getiterator()
-tag = note.findall("student")
-print(tag)
-if tag[0].text:
-    print('true')
-else:
-    print('false')
+p = re.compile('\s+')
+p_num = re.compile('\d+')
+text_list = list(note.itertext())
+for text in text_list:
+    m=p.match(text)
+    m_num = p_num.match(text)
+    if not m and m_num:
+        print('age: '+text)
+    elif not m and not m_num:
+        print('major: '+text)
