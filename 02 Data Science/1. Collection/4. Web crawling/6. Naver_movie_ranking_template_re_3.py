bcb1931f948742c0fa9b84ad33e95313bf4b0e72
민선 <alstjs7321@naver.com>
2019-02-27 20:25:44 +0900

    at home

--- 02 Data Science/1. Collection/4. Web crawling/6. Naver_movie_ranking_template_re_3.py
+++ 02 Data Science/1. Collection/4. Web crawling/6. Naver_movie_ranking_template_re_3.py
@@ -6,28 +6,22 @@ html = urllib.request.urlopen('http://movie.naver.com/movie/sdb/rank/rmovie.nhn'
 soup = BeautifulSoup(html,'html.parser')
 soup = str(soup)
 tags_list = ['순위,영화명,변동폭']
-# p_title = re.compile(r'<a href="/.*" title="(.+)">\1</a>$',re.MULTILINE)
-# p_updown = re.compile('<td class="ac">.+ alt="([a-z]+)"',re.MULTILINE)
-# p_change = re.compile('<td class="range ac">(\d)',re.MULTILINE)
-p = re.compile(r'<a href="/.*" title="(.+)">\1</a>.+<td class="ac">.+ alt="([a-z]+)".+<td class="range ac">(\d)',re.M)
-
+p=re.compile(r'title="(.+)">\1</a>\n</div>\n</td>\n.+\n.+\n<td class="ac">.+alt="([a-z]+)".+\n<td class=.+(\d)')
 match_list = p.findall(soup)
-print(match_list)
-# for match in match_list:
-#     print(match)
-# for index in range(len(title_list)):
-#     if title_list[index].find(',')!=-1:
-#         title = '"'+title_list[index]+'"'
-#     else:
-#         title = title_list[index]
-#     if updown_list[index]=='up':
-#         change = '+'+change_list[index]
-#     elif updown_list[index]=='down':
-#         change = '-'+change_list[index]
-#     else:
-#         change = change_list[index]
-#     tags_list.append(str(index + 1) + ',' + title + ',' + change)
-#
-# f = open('Naver_movie_ranking_re_3.csv','w')
-# f.write('\n'.join(tags_list))
-# f.close()
+
+for index in range(len(match_list)):
+    if match_list[index][0].find(',')!=-1:
+        title = '"'+match_list[index][0]+'"'
+    else:
+        title = match_list[index][0]
+    if match_list[index][1]=='up':
+        change = '+'+match_list[index][2]
+    elif match_list[index][1]=='down':
+        change = '-'+match_list[index][2]
+    else:
+        change = match_list[index][2]
+    tags_list.append(str(index + 1) + ',' + title + ',' + change)
+
+f = open('Naver_movie_ranking_re_3.csv','w')
+f.write('\n'.join(tags_list))
+f.close()
