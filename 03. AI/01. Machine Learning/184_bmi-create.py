a86b59dbdbdfe2d26d8075e74398f8a05638c1f1
Yoonminsun <alstjs7321@naver.com>
2019-03-25 16:07:00 +0900

    집에서 수정1

--- 03. AI/01. Machine Learning/184_bmi-create.py
+++ 03. AI/01. Machine Learning/184_bmi-create.py
@@ -6,7 +6,7 @@ def calc_bmi(h, w):
     if bmi < 25: return "normal"
     return "fat"
 # 출력 파일 준비하기
-fp = open("bmi.csv","w",encoding="utf-8")
+fp = open("bmi.csv","w",encoding="utf-8",newline='')
 fp.write("height,weight,label\r\n")
 # 무작위로 데이터 생성하기
 cnt = {"thin":0, "normal":0, "fat":0}
--- 03. AI/01. Machine Learning/test.py
+++ 03. AI/01. Machine Learning/test.py
@@ -1,20 +1,20 @@
 import pandas as pd
-import random,re
+import sklearn.cluster, sklearn.preprocessing
+from sklearn import metrics
 
-## DataFrame 으로 읽어들인 후 리스트로 만들어 shuffle 하기
-house = pd.read_csv('Housing.csv',sep=',',header=0)
-house_list=[]
-for num in range(len(house)):
-    data=[]
-    for num2 in range(len(house.values[num])):
-        data.append(house.values[num][num2])
-    house_list.append(data)
-random.shuffle(house_list)
+alco = pd.read_csv('niaaa-report2009.csv',index_col='State')
 
+columns = ['Wine','Beer']
 
-# random.shuffle(house)
-# total_len = len(house)
-# train_len = int(total_len * 2/3)
-# house = house.loc[0:train_len,'price']
-# house = house.loc[train_len+1:total_len,'price']
-# print(house)
+kmeans = sklearn.cluster.KMeans(n_clusters=9)
+kmeans.fit(alco[columns])
+alco['Cluster'] = kmeans.labels_
+alco.to_csv('Clustering_Result.csv',index=False)
+
+data = alco[['Wine','Beer']]
+label = alco['Cluster']
+
+predicted = kmeans.predict(data)
+print('예측결과\n',predicted)
+ac_score = round(metrics.accuracy_score(label,predicted)*100,2)
+print('정답률 = %s %%'%ac_score)
