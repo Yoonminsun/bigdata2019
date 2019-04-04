0782c976844a83247b6390cb6f0e437aefabf98e
Yoonminsun <alstjs7321@naver.com>
2019-04-04 13:06:52 +0900

    집에서 수정1

--- image/opencv_topixel.py
+++ image/opencv_topixel.py
@@ -1,18 +1,23 @@
 import numpy as np
 import cv2
-img = cv2.imread('test.png')
+img = cv2.imread('zero.jpg',cv2.IMREAD_GRAYSCALE)
 cv2.imshow('imageshow',img)
 cv2.waitKey(0) # 이미지가 show 된 상태로 wait
 cv2.destroyAllWindows()
 
-pixel = []
+# pixel = []
 
-h,w,cha = img.shape
-for y in range(0,h):
-    for x in range(0,w):
-        r = img.item(y,x,2)
-        g = img.item(y,x,1)
-        b = img.item(y,x,0)
-        pixel.append([r,g,b])
+# h,w = img.shape
+# for y in range(0,h):
+#     for x in range(0,w):
+#         # grayscale 인 경우
+#         pixel.append(img.item(y,x))
+#         # rgb 인 경우
+#         # r = img.item(y,x,2)
+#         # g = img.item(y,x,1)
+#         # b = img.item(y,x,0)
+#         # pixel.append([r,g,b])
+#
+# print(pixel)
 
-print(pixel)
+orb = cv2.ORB_create()
--- image/pillow_topixel.py
+++ image/pillow_topixel.py
@@ -1,8 +1,11 @@
 import PIL.Image as pilimg
+from PIL import ImageFilter
 import numpy as np
 
-im = pilimg.open('zero.jpg')
+im = pilimg.open('card.png')
 im.show()
+cont_im = im.filter(ImageFilter.CONTOUR)
+cont_im.show()
 pix = np.array(im)
 print(pix) # 437*626 >> pix[0]은 1열의 437행의 r,g,b 값들을 가진다
 
